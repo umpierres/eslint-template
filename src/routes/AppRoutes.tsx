64e549ad9fff02bf28b699f8b0e929d448a61a46
umpierres <alexandraumpierres@outlook.com>
2023-04-11 19:01:02 -0300

    iniciando template

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const AppRoutes: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default AppRoutes;
